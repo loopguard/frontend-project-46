1b3640b508c0d978e20db9b39bc0a870f848db02
Aleksandr Efimov <avefimov@avito.ru>
2023-03-23 18:53:39 +0200

    step_5

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 [![Node CI](https://github.com/loopguard/frontend-project-46/workflows/Node%20CI/badge.svg)](https://github.com/loopguard/frontend-project-46/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/e47f96788897b183ea33/maintainability)](https://codeclimate.com/github/loopguard/frontend-project-46/maintainability)
 [![Test Coverage](https://api.codeclimate.com/v1/badges/e47f96788897b183ea33/test_coverage)](https://codeclimate.com/github/loopguard/frontend-project-46/test_coverage)
+
+#### Find diff between two files
+[![asciicast](https://asciinema.org/a/oH7WvRHiIzwerAMCzrcFlslGj.svg)](https://asciinema.org/a/oH7WvRHiIzwerAMCzrcFlslGj)
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,17 +7,25 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
+const expectedOut = '{\n'
+  + '      - follow: false\n'
+  + '        host: hexlet.io\n'
+  + '      - proxy: 123.234.53.22\n'
+  + '      - timeout: 50\n'
+  + '      + timeout: 20\n'
+  + '      + verbose: true\n'
+  + ' }';
+
 test('success json diff', () => {
   const filename1 = getFixturePath('file1.json');
   const filename2 = getFixturePath('file2.json');
-  const expectedJson = '{\n'
-        + '      - follow: false\n'
-        + '        host: hexlet.io\n'
-        + '      - proxy: 123.234.53.22\n'
-        + '      - timeout: 50\n'
-        + '      + timeout: 20\n'
-        + '      + verbose: true\n'
-        + ' }';
 
-  expect(genDiff(filename1, filename2)).toBe(expectedJson);
+  expect(genDiff(filename1, filename2)).toBe(expectedOut);
+});
+
+test('success yml diff', () => {
+  const filename1 = getFixturePath('file1.yml');
+  const filename2 = getFixturePath('file2.yml');
+
+  expect(genDiff(filename1, filename2)).toBe(expectedOut);
 });
